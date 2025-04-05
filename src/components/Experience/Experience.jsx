38ff9191e8acea4c44528a321bf21a3b5df824ca
neelesh <neeleshvinchoorkar@gmail.com>
2025-04-06 04:38:56 +0530

    website revamped

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,44 +1,44 @@
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
-import { getImageUrl } from "../../utils";
+import { getImageUrl } from "../../utils"
 
 export const Experience = () => {
   return (
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience</h2>
       <div className={styles.content}>
-        <ul className={styles.history}>
-          {history.map((historyItem, id) => {
-            return (
-              <li key={id} className={styles.historyItem}>
-                <img className={styles.companyImg} src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
-                <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role},`}</h3>
-                  <h2>{`${historyItem.organisation}`}</h2>
-                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li key={id}>{experience}</li>;
-                    })}
-                  </ul>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-        <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
+        {/* TIMELINE EXPERIENCE SECTION */}
+        <div className={styles.timelineWrapper}>
+          {history.map((item, id) => (
+            <div key={id} className={styles.timelineItem}>
+              <div className={styles.timelineDot}></div>
+              <div className={styles.timelineContent}>
+                <h3>{item.role}</h3>
+                <h4>{item.organisation}</h4>
+                <span className={styles.dates}>
+                  {item.startDate} - {item.endDate}
+                </span>
+                <ul>
+                  {item.experiences.map((point, index) => (
+                    <li key={index}>{point}</li>
+                  ))}
+                </ul>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
 
+        {/* SKILLS SECTION */}
+        <div className={styles.skills}>
+          {skills.map((skill, id) => (
+            <div key={id} className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 export const ThemeContext = createContext();
+
+// eslint-disable-next-line react/prop-types
 export const ThemeProvider = ({ children }) => {
   // const [dark, setDark] = useState("");
   const [storedValue, setStoredValue] = useLocalStorage("theme", "");
